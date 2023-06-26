5f53650387d87eb05b2bb2aa487c90924cdb433e
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-06-26 16:46:16 -0700

    signin page and route creation

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,77 @@
+import startDb from "@/lib/db";
+import UserModel from "@/models/userModel";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+
+// sign-in route creation
+const authOptions: NextAuthOptions = {
+  // json web token (jwt) session provided for login 
+  session: {
+      strategy: "jwt"
+  }, 
+  
+  // need to import CredentialsProvider to pass in documentation from nextjs 
+  providers: [
+    // since using email and passwords, we'll need and use CredentialsProvider
+      CredentialsProvider({
+          type: "credentials",
+          // leave object empty since we don't want a default form 
+          credentials: {},
+          // authorize function will fire when sending sign-in request from backend api 
+          // credentials holds all incoming data from frontend. req makes HTTP requests and handles incoming data thru json 
+          async authorize(credentials, req) {
+              // destructuring credentials once passed in and casting incoming data as strings
+              const { email, password } = credentials as {
+                  email: string;
+                  password: string;
+              };
+              // perform loging logic and/or find out user from db
+                // upon capturing email and password, start and connect db to check for matching user email
+              await startDb();
+              const user = await UserModel.findOne({ email });
+
+                // if there's no user, throw error and notify user
+              if (!user) throw Error("email/password mismatch")
+
+                // if user does exist, then compare the password
+              const passwordMatch = await user.comparePassword(password);
+              if (!passwordMatch) throw Error("email/password mismatch");
+
+              // if user's login info is valid, return user object containing user's info (authenticated user)
+              return { 
+                  name: user.name, 
+                  email: user.email,
+                  role: user.role,
+                  id: user._id
+              };
+          },
+      }),
+  ],
+
+  // to get same user response inside application, need a callback function
+  callbacks: {
+    // callback functions run the first time we send a sign-in request
+    jwt(params: any) {
+      // check if there's a user and update token by assigning user's role and id. Then return token
+      if (params.user?.role) {
+        params.token.role = params.user.role;
+        params.token.id = params.user.id;
+      }
+      return params.token;
+    },
+
+    session({ session, token }) {
+      // check if there's a user and update session by adding user's id and role. Then return session
+      if (session.user) {
+        (session.user as { id: string }).id = token.id as string;
+        (session.user as { role: string }).role = token.role as string; 
+      }
+      return session;
+    },
+  },
+};
+
+const authHandler = NextAuth(authOptions);
+
+export { authHandler as GET, authHandler as POST }
---
+++ app/api/auth/users/route.ts
@@ -0,0 +1,53 @@
+import startDb from "@/lib/db";
+import UserModel from "@/models/userModel";
+import { NextResponse } from "next/server";
+
+
+interface NewUserRequest {
+  // request will contain use info coming from frontend
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface NewUserResponse {
+  // response will contain data we send as a response
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+}
+
+
+// with the next response, either return the user or an error
+type NewResponse = NextResponse<{ user?: NewUserResponse; error?: string}>;
+
+// take the incoming request(user info) and return a promise with the new response(user info)
+export const POST = async (req: Request): Promise<NewResponse> => {
+  // request the data coming from the frontend, then compare incoming data to what's in our db
+  const body = (await req.json()) as NewUserRequest;
+
+  await startDb();
+
+  const oldUser = await UserModel.findOne({ email: body.email });
+  // if user already exists, throw error notifying user about email is in use
+  if (oldUser) 
+    return NextResponse.json(
+      { error: "email is already in user" },
+      { status: 422 }
+    );
+  
+  // if user doesn't exist, create a new user with the UserModel
+  const user = await UserModel.create({ ...body });
+
+  // once user has been created, return a NextResponse with new user's info
+  return NextResponse.json({
+    user: {
+      id: user._id.toString(),
+      email: user.email,
+      name: user.name,
+      role: user.role,
+    },
+  });
+};
+
