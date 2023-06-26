74011c4385fbf713957fa15c64e7394268ae5da2
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-06-25 18:29:38 -0700

    created navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+import AuthProfileMenu from "../AuthProfileMenu";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center max-w-screen-lg mx-auto px-5 py-2 shadow-md justify-between mt-4 rounded">
+      <Link href="/">Home</Link>      
+      <AuthProfileMenu/>
+    </nav>
+  )
+}
