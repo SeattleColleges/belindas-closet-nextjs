bd09f8df12d0c3048fc918eaffc224dbe70d4866
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-06-28 16:28:00 -0700

    "SessionProvider added.. req for useSession hooks"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/Navbar';
+import SessionProvider from '@/components/SessionProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>{children}</body>
+        <SessionProvider>
+          <Navbar/>{children}
+        </SessionProvider>
+      </body>
     </html>
   )
 };
