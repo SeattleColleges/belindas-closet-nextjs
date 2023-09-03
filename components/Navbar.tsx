c931c571899e0e0e9c7b85fbde0f76d062c85e68
taylorpapke <94101953+taylorpapke@users.noreply.github.com>
2023-09-03 12:39:28 -0700

    moved sign-in, sign-up, forgot-password to auth folder. Created auth profile menu. (#78)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,18 @@
+import Profile from "@/app/profile/page";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import AuthProfileMenu from "./AuthProfileMenu";
 import Image from 'next/image';
 import logo from '../app/logo.png';
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-wrap items-center max-w-screen-lg justify-between mx-auto p-4 bg-">
+    <nav className="flex items-center max-w-screen-lg mx-auto px-5 py-2 shadow-md justify-between mt-2 mb-2 rounded">
       <a href="/"><Image src={logo} alt="logo" width={40} height={40}/></a>
-      <div className="flex space-x-4">
-        <Link href="/">Home</Link>
-        <Link href="/api/auth/product-page">Add Product</Link>
-      </div>
+      <Link href="/">Home</Link>
+      <Link href="/auth/sign-in">sign in</Link>
+      <Link href="/auth/sign-up">sign up</Link>
+      <AuthProfileMenu />      
     </nav>
   )
 };
