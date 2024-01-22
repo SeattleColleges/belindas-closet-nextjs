36f181ce8e45b88c381f353c1aca8cbadf7463d9
Edwin Ho <edwinh300@gmail.com>
2024-01-21 21:40:51 -0800

    added place holder creator's page

---
+++ app/creator-page/page.tsx
@@ -0,0 +1,11 @@
+const Creator = () => {
+    // Temporary boilerplate code to make it compile
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1>Placeholder creators page.</h1>
+            <p>Place holder text to fill in the space.</p>
+        </div>
+    );
+};
+
+export default Creator;
