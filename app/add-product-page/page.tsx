beaefdd55dcef60e827114fad0c3a7f43fd61e67
Tin Pham <nhattin55@gmail.com>
2024-01-22 12:19:43 -0800

    update add product function

--- app/add-product-page/page.tsx
+++ app/add-product-page/page.tsx
@@ -4,46 +4,47 @@ import React, { useState } from "react";
 import styles from "./page.module.css";
 import InputSelect from "@/components/InputSelect";
 import Input from "@/components/Input";
-
+import {
+  ProductTypeList,
+  ProductGenderList,
+  ProductSizeShoeList,
+  ProductSizesList,
+  ProductSizePantsWaistList,
+  ProductSizePantsInseamList,
+} from "./product-prop-list";
 const AddProduct = () => {
-
-  enum ProductType {
-    Shoes = "Shoes",
-    Shirts = "Shirts",
-    Pants = "Pants",
-    Skirt = "Skirt",
-    JacketBlazer = "Jacket/Blazer",
-    Dress = "Dress",
-    CasualWear = "Casual Wear",
-    Suits = "Suits",
-    Accessories = "Accessories"
-  }
-
-  const [productType, setProductType] = useState(ProductType.Shoes);
-  const [productGender, setProductGender] = useState("MALE");
-  const [productSizeShoe, setProductSizeShoe] = useState(5);
+  const [productType, setProductType] = useState(ProductTypeList.Shoes);
+  const [productGender, setProductGender] = useState(ProductGenderList.MALE);
+  const [productSizeShoe, setProductSizeShoe] = useState(
+    ProductSizeShoeList[0]
+  );
   const [productSizes, setProductSizes] = useState("SELECT_SIZE");
-  const [productSizePantsWaist, setProductSizePantsWaist] = useState(28);
-  const [productSizePantsInseam, setProductSizePantsInseam] = useState(28);
+  const [productSizePantsWaist, setProductSizePantsWaist] = useState(
+    ProductSizePantsWaistList[0]
+  );
+  const [productSizePantsInseam, setProductSizePantsInseam] = useState(
+    ProductSizePantsInseamList[0]
+  );
   const [productDescription, setProductDescription] = useState("");
   const [productImage, setProductImage] = useState("");
 
   const handleProductTypeSelect = (e: {
-    target: { value: React.SetStateAction<ProductType> };
+    target: { value: React.SetStateAction<ProductTypeList> };
   }) => {
     setProductType(e.target.value);
-  }
+  };
 
   const handleProductGenderSelect = (e: {
-    target: { value: React.SetStateAction<string> };
+    target: { value: React.SetStateAction<ProductGenderList> };
   }) => {
     setProductGender(e.target.value);
   };
 
   const handleProductSizeShoeSelect = (e: {
-    target: { value: React.SetStateAction<any> };
+    target: { value: React.SetStateAction<number> };
   }) => {
-    setProductSizeShoe(e.target.value);
+    const newValue = parseInt(e.target.value.toString(), 10);
+    setProductSizeShoe(newValue);
   };
 
   const handleProductSizeSelect = (e: {
@@ -53,15 +54,17 @@ const AddProduct = () => {
   };
 
   const handleProductSizePantsWaistSelect = (e: {
-    target: { value: React.SetStateAction<any> };
+    target: { value: React.SetStateAction<number> };
   }) => {
-    setProductSizePantsWaist(e.target.value);
+    const newValue = parseInt(e.target.value.toString(), 10);
+    setProductSizePantsWaist(newValue);
   };
 
   const handleProductSizePantsInseamSelect = (e: {
-    target: { value: React.SetStateAction<any> };
+    target: { value: React.SetStateAction<number> };
   }) => {
-    setProductSizePantsInseam(e.target.value);
+    const newValue = parseInt(e.target.value.toString(), 10);
+    setProductSizePantsInseam(newValue);
   };
 
   const handleDescriptionChange = (e: {
@@ -82,7 +85,8 @@ const AddProduct = () => {
       const res = await fetch("http://localhost:3000/api/products/new", {
         method: "POST",
         headers: {
-          Authorization: "Bearer " + process.env.NEXT_PUBLIC_API_KEY// JWT Token Here,
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + process.env.NEXT_PUBLIC_API_KEY,
         },
         body: JSON.stringify({
           productType,
@@ -99,6 +103,8 @@ const AddProduct = () => {
       if (!res.ok) {
         throw new Error(res.statusText);
       } else {
+        const data = await res.json();
+        console.log(data);
         alert("Product Added!");
       }
     } catch (error) {
@@ -116,121 +122,93 @@ const AddProduct = () => {
         <InputSelect
           label="Product Type"
           value={productType}
-          options={[
-            { label: "Shoes", value: ProductType.Shoes },
-            { label: "Shirts", value: ProductType.Shirts },
-            { label: "Pants", value: "Pants" },
-            { label: "Skirt", value: "Skirt" },
-            { label: "Jacket/Blazer", value: "Jacket/Blazer" },
-            { label: "Dress", value: "Dress" },
-            { label: "Casual Wear", value: "Casual Wear" },
-            { label: "Suits", value: "Suits" },
-            { label: "Accessories", value: "Accessories" },
-          ]}
+          options={Object.values(ProductTypeList).map((type) => ({
+            label: type,
+            value: type,
+          }))}
           onChange={handleProductTypeSelect}
           style={{ color: "black" }}
         />
 
         {/* Product Gender Field */}
         <InputSelect
-        label="Product Gender"
-        value={productGender}
-        options={[
-          {label: "Male", value: "MALE"},
-          {label: "Female", value: "FEMALE"},
-          {label: "Non-Binary", value: "NON_BINARY"}
-        ]}
-        onChange={handleProductGenderSelect}
-        style={{color: "black"}}
+          label="Product Gender"
+          value={productGender}
+          options={Object.values(ProductGenderList).map((gender) => ({
+            label: gender,
+            value: gender,
+          }))}
+          onChange={handleProductGenderSelect}
+          style={{ color: "black" }}
         />
 
         {/* Product Size Shoe Field */}
         <InputSelect
-        label="Product Size Shoe"
-        value={productSizeShoe}
-        options={[
-          {label: "5", value: 5},
-          {label: "6", value: 6},
-          {label: "7", value: 7},
-          {label: "8", value: 8},
-          {label: "9", value: 9},
-          {label: "10", value: 10},
-          {label: "11", value: 11},
-          {label: "12", value: 12},
-        ]}
-        onChange={handleProductSizeShoeSelect}
-        style={{color: "black"}}
+          label="Product Size Shoe"
+          value={productSizeShoe}
+          options={Object.values(ProductSizeShoeList).map((size) => ({
+            label: size,
+            value: size,
+          }))}
+          onChange={handleProductSizeShoeSelect}
+          style={{ color: "black" }}
         />
 
         {/* Product Size Field */}
         <InputSelect
-        label="Product Size"
-        value={productSizes}
-        options={[
-          {label: "SELECT_SIZE", value: "SELECT_SIZE"},
-          {label: "XS", value: "XS"},
-          {label: "S", value: "S"},
-          {label: "M", value: "M"},
-          {label: "L", value: "L"},
-          {label: "XL", value: "XL"},
-          {label: "XXL", value: "XXL"},
-        ]}
-        onChange={handleProductSizeSelect}
-        style={{color: "black"}}
+          label="Product Size"
+          value={productSizes}
+          options={[
+            { label: "Select Size", value: "SELECT_SIZE" },
+            ...Object.values(ProductSizesList).map((size) => ({
+              label: size,
+              value: size,
+            })),
+          ]}
+          onChange={handleProductSizeSelect}
+          style={{ color: "black" }}
         />
 
         {/* Product Size Pants Waist Field */}
         <InputSelect
-        label="Product Size Pants Waist"
-        value={productSizePantsWaist}
-        options={[
-          {label: "28", value: 28},
-          {label: "30", value: 30},
-          {label: "32", value: 32},
-          {label: "34", value: 34},
-          {label: "36", value: 36},
-          {label: "38", value: 38},
-          {label: "40", value: 40},
-          {label: "42", value: 42},
-        ]}
-        onChange={handleProductSizePantsWaistSelect}
-        style={{color: "black"}}
+          label="Product Size Pants Waist"
+          value={productSizePantsWaist}
+          options={Object.values(ProductSizePantsWaistList).map((size) => ({
+            label: size,
+            value: size,
+          }))}
+          onChange={handleProductSizePantsWaistSelect}
+          style={{ color: "black" }}
         />
 
         {/* Product Size Pants Inseam Field */}
         <InputSelect
-        label="Product Size Pants Inseam"
-        value={productSizePantsInseam}
-        options={[
-          {label: "28", value: 28},
-          {label: "30", value: 30},
-          {label: "32", value: 32},
-          {label: "34", value: 34},
-          {label: "36", value: 36},
-          {label: "38", value: 38},
-          {label: "40", value: 40},
-          {label: "42", value: 42},
-        ]}
-        onChange={handleProductSizePantsInseamSelect}
-        style={{color: "black"}}
+          label="Product Size Pants Inseam"
+          value={productSizePantsInseam}
+          options={Object.values(ProductSizePantsInseamList).map((size) => ({
+            label: size,
+            value: size,
+          }))}
+          onChange={handleProductSizePantsInseamSelect}
+          style={{ color: "black" }}
         />
 
         {/* Product Description Field */}
         <Input
-        label="Product Description"
-        value={productDescription}
-        type="text"
-        onChange={handleDescriptionChange}
-        style={{color: "black"}}
+          label="Product Description"
+          value={productDescription}
+          type="text"
+          onChange={handleDescriptionChange}
+          style={{ color: "black" }}
         />
 
         {/* Product Upload Image Field */}
         <Input
-        label="Product Image"
-        value={productImage}
-        type="file"
-        onChange={handleImageUpload}
-        style={{color: "white"}}
+          label="Product Image"
+          value={productImage}
+          type="file"
+          onChange={handleImageUpload}
+          style={{ color: "white" }}
         />
 
         {/* Submit Button */}
