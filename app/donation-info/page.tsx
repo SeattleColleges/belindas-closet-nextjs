22583a1cfd84a40a4e9d88bc25f397295cffb4ee
E. Brink <80282265+brinkbrink@users.noreply.github.com>
2024-01-24 21:01:32 -0800

    Delete app/donation-info directory

--- app/donation-info/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./page.module.css";
-
-const DonationInfo=()=>{
-    return <div className={styles.container}>
-                <h1 className={styles.title}>Donation Info</h1>
-            </div>
-}
-
-export default DonationInfo;
