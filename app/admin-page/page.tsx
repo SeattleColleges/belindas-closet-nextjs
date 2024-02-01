139d52f09015517a06e5fea9b5ffaf0904af51c8
taylorpapke <taylorpapke@gmail.com>
2024-02-01 02:38:47 -0800

    added back export and commented out incompatible link tag and import

--- app/admin-page/page.tsx
+++ app/admin-page/page.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
+// import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
 
 const Admin = () => {
   const handleAddProduct = () => {
@@ -9,21 +9,23 @@ const Admin = () => {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1> Wellcome to the PAGE      </h1>
       <p>FIX: allow only users with admin role to be routed to this page</p>
-      <Link to="/add-product-page">
+      {/* <Link to="/add-product-page"> */}
         <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}>
           Add Product
         </button>
-      </Link>
-      <Link to="/add-product-page">
+      {/* </Link> */}
+      {/* <Link to="/add-product-page"> */}
         <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}>
           All Products
         </button>
-      </Link>
-      <Link to="/add-product-page">
+      {/* </Link> */}
+      {/* <Link to="/add-product-page"> */}
         <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}>
           Edit User Roles
         </button>
-      </Link>
+      {/* </Link> */}
     </div>
   );
 };
+
+export default Admin;
