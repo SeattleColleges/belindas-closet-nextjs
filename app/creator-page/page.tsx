a19a0843008a9c60684f691a2a2ebaa2e8ae512b
krayzieeddi <63763467+krayzieeddi@users.noreply.github.com>
2024-02-04 15:48:53 -0800

    added all products and add products buttons to creator page (#193)

--- app/creator-page/page.tsx
+++ app/creator-page/page.tsx
@@ -1,7 +1,25 @@
+import styles from "../home.module.css";
+
 const Creator = () => {
     // Temporary boilerplate code to make it compile
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
+       
+        <div >
+            <div className={styles.loginButton}>
+                <a href="/">
+                <button>All Products</button>
+                </a>
+            </div>
+            
+            <div className={styles.loginButton}>
+                <a href="/add-product-page">
+                <button>Add Products</button>
+                </a>
+            </div>
+
+        </div>
+
             <h1>Placeholder creators page.</h1>
             <p>Place holder text to fill in the space.</p>
         </div>
