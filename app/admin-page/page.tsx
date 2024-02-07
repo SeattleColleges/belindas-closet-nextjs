44d5e99a6aa34f670597214a001854a4883ed1ba
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-02-06 19:51:38 -0800

    Belindas-closet-nextjs-2-199-add-buttons-to-admin-page (#190)
    * the mongodb has problem
    
    * rename file and remove commented out code
    
    * Added The Creator Page
    
    * added the buttons to the Admin Page
    
    * Added Buttons to the Admin Page
    
    * added back export and commented out incompatible link tag and import
    
    * Delete app/creator/page.tsx
    
    ---------
    
    Co-authored-by: Tin Pham <nhattin55@gmail.com>
    Co-authored-by: taylorpapke <94101953+taylorpapke@users.noreply.github.com>
    Co-authored-by: taylorpapke <taylorpapke@gmail.com>

--- app/admin-page/page.tsx
+++ app/admin-page/page.tsx
@@ -1,10 +1,29 @@
+// import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
+
 const Admin = () => {
+  const handleAddProduct = () => {
+    // Add code here to handle the "Add Product" button click event
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1>
-        Placeholder for the admin page so npm run build compiles successfully.
-      </h1>
+      <h1> Wellcome to the PAGE      </h1>
       <p>FIX: allow only users with admin role to be routed to this page</p>
+      {/* <Link to="/add-product-page"> */}
+        <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}>
+          Add Product
+        </button>
+      {/* </Link> */}
+      {/* <Link to="/add-product-page"> */}
+        <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}>
+          All Products
+        </button>
+      {/* </Link> */}
+      {/* <Link to="/add-product-page"> */}
+        <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}>
+          Edit User Roles
+        </button>
+      {/* </Link> */}
     </div>
   );
 };
