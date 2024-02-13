3ac3f341e959936d81a55af6271c055c322a5014
MuhammadouDrammeh <117953848+MuhammadNSC@users.noreply.github.com>
2024-02-13 02:29:52 -0800

    Belinda_NextJS_Sprint_1_166_Password_Toggle (#191)
    * Added Show/Hide Password Input Field
    
    * Text Color Changed To Black
    
    * Implemented password visibility toggle with MUI
    
    ---------
    
    Co-authored-by: Muhammadou Drammeh <Muhammadou1drammeh@gmail.com>
    Co-authored-by: cshimm <cameronjshimmin@gmail.com>

--- components/InputFields.tsx
+++ components/InputFields.tsx
@@ -13,7 +13,7 @@ const InputField: React.FC<InputFieldProps> = ({ label, type, name, value, onCha
     <div className="mb-4">
       <label className="block mb-1 font-semibold">{label}</label>
       <input
-        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-black"
         type={type}
         name={name}
         value={value}
