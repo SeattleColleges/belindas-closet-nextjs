fff28fc96f78054f55a984c1d61f45cccba2fed2
Tin Pham <72054441+tinpham5614@users.noreply.github.com>
2024-02-22 19:27:24 -0800

    belinda-nextjs-225-delete_archive_buttons-01 (#226)
    * update product page
    
    * update icons inline

---
+++ app/category-page/[categoryId]/products/[productId]/ProductDetailDisplay.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import logo from "app/logo.png";
+import {
+  Box,
+  Button,
+  Container,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import ArchiveIcon from "@mui/icons-material/Archive";
+
+interface Product {
+  _id: string;
+  productType: string[];
+  productGender: string[];
+  productShoeSize: string[];
+  productSize: string[];
+  productSizePantsWaist: string[];
+  productSizePantsInseam: string[];
+  productDescription: string;
+  productImage: string;
+}
+
+const ProductDetailDisplay = ({ product }: { product: Product }) => {
+  return (
+    <Container
+      fixed
+      maxWidth="lg"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        bgcolor: "#12202d",
+      }}
+    >
+      <Box
+        width={800}
+        display="flex"
+        alignItems="center"
+        flexDirection="column"
+        gap={2}
+        bgcolor="#293745"
+        p={3}
+      >
+        <Typography component="h1" variant="h4">
+          {product && product.productType}
+        </Typography>
+        <Paper elevation={3}>
+          <Box p={2} m={2}>
+            {product && (
+              <Stack direction="column" spacing={2} justifyContent="center">
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  gap={2}
+                  alignItems="flex-start"
+                >
+                  <Typography variant="h6">
+                    Product ID: {product._id}
+                  </Typography>
+                  <Typography variant="h6">
+                    Product Type: {product.productType}
+                  </Typography>
+                  <Typography variant="h6">
+                    Product Gender: {product.productGender}
+                  </Typography>
+                  <Typography variant="h6">
+                    Product Shoe Size: {product.productShoeSize || "N/A"}
+                  </Typography>
+                  <Typography variant="h6">
+                    Product Size: {product.productSize || "N/A"}
+                  </Typography>
+                  <Typography variant="h6">
+                    Product Size Pants Waist:{" "}
+                    {product.productSizePantsWaist || "N/A"}
+                  </Typography>
+                  <Typography variant="h6">
+                    Product Size Pants Inseam:{" "}
+                    {product.productSizePantsInseam || "N/A"}
+                  </Typography>
+                  <Typography variant="h6">
+                    Product Description: {product.productDescription || "N/A"}
+                  </Typography>
+                </Box>
+                <Box display="flex" justifyContent="center">
+                  <Image
+                    src={logo} // temporary image
+                    alt="Product Image"
+                    width={250}
+                    height={250}
+                    style={{ borderRadius: "5px", marginTop: "10px" }}
+                  />
+                </Box>
+              </Stack>
+            )}
+          </Box>
+
+          <Stack direction="row" spacing={2} justifyContent="center">
+            {/* Edit Button */}
+            <Box p={2} display="flex" justifyContent="center">
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<EditIcon />}
+              >
+                Edit
+              </Button>
+            </Box>
+            {/* Delete Button */}
+            <Box p={2} display="flex" justifyContent="center">
+              <Button
+                variant="contained"
+                color="error"
+                startIcon={<DeleteIcon />}
+              >
+                Delete
+              </Button>
+            </Box>
+            {/* Archive Button */}
+            <Box p={2} display="flex" justifyContent="center">
+              <Button
+                variant="contained"
+                color="warning"
+                startIcon={<ArchiveIcon />}
+              >
+                Archive
+              </Button>
+            </Box>
+          </Stack>
+        </Paper>
+      </Box>
+    </Container>
+  );
+};
+
+export default ProductDetailDisplay;
--- app/category-page/[categoryId]/products/[productId]/page.tsx
+++ app/category-page/[categoryId]/products/[productId]/page.tsx
@@ -1,18 +1,31 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import ProductDetailDisplay from "../[productId]/ProductDetailDisplay";
 
 interface Product {
   _id: string;
   productType: string[];
   productGender: string[];
+  productShoeSize: string[];
+  productSize: string[];
+  productSizePantsWaist: string[];
+  productSizePantsInseam: string[];
   productDescription: string;
-  // Add other fields as needed
+  productImage: string;
 }
 
-const ProductDetail = ({ params }: { params: { categoryId: string, productId: string } }) => {
+const ProductDetail = ({
+  params,
+}: {
+  params: { categoryId: string; productId: string };
+}) => {
   const [product, setProduct] = useState<Product | null>(null); // State to hold the fetched product details
-  const [isLoading, setIsLoading] = useState(true); // State to track the loading status
+  const [isLoading, setIsLoading] = useState(false); // State to hold loading status
   const [error] = useState(null); // State to hold any error that occurs during fetching
 
   const { categoryId, productId } = params;
@@ -25,18 +38,20 @@ const ProductDetail = ({ params }: { params: { categoryId: string, productId: st
       setIsLoading(true);
       try {
         const response = await fetch(fetchUrl, {
-          method: 'GET',
+          method: "GET",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         });
         if (!response.ok) {
-          throw new Error(`Error fetching product details: ${response.statusText}`);
+          throw new Error(
+            `Error fetching product details: ${response.statusText}`
+          );
         }
         const data: Product = await response.json();
         setProduct(data);
       } catch (error) {
-        console.error('Error getting product detail:', error);
+        console.error("Error getting product detail:", error);
       } finally {
         setIsLoading(false);
       }
@@ -45,25 +60,15 @@ const ProductDetail = ({ params }: { params: { categoryId: string, productId: st
     fetchProductDetails();
   }, [decodedCategoryId, productId]); // Dependency array to trigger effect when categoryId or productId changes
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <CircularProgress />
+      </Box>
+    );
   if (error) return <div>Error: {error}</div>;
 
-  return (
-    <div>
-      <h1>Product Page</h1>
-      {/* Check if product is not null before accessing its properties */}
-      {product ? (
-        <div>
-          <p>Product ID: {product._id}</p>
-          <p>Product Type: {product.productType.join(', ')}</p>
-          <p>Product Gender: {product.productGender.join(', ')}</p>
-          <p>Description: {product.productDescription}</p>
-        </div>
-      ) : (
-        <p>Product details not available.</p>
-      )}
-    </div>
-  );
+  return <ProductDetailDisplay product={product as Product} />;
 };
 
 export default ProductDetail;
