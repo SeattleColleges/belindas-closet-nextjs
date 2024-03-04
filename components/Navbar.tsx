5b8fa0680e71f1339b62d03ba167716b273cedbd
Muhammadou Drammeh <muhammadou.drammeh@seattlecolleges.edu>
2024-03-04 14:29:53 -0800

    Added Profile Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -120,124 +120,6 @@
 // }
 
 
-
-// "use client";
-
-// import React from "react";
-// import Image from "next/image";
-// import logo from "../app/logo.png";
-// import { AppBar, Button, IconButton, Toolbar, Grid, Drawer, Box, CssBaseline, Divider, List } from "@mui/material";
-// import MenuIcon from "@mui/icons-material/Menu";
-// import Link from "next/link";
-// import CategoryDropDownMenu from "./CategoryDropDownMenu"; // Import CategoryDropDownMenu component
-// import ProfileDropDownMenu from "./ProfileDropDownMenu"; // Import ProfileDropDownMenu component
-
-// const drawerWidth = 240;
-// const navItems = ["Home", "Sign In", "Donation"];
-// const links = ["/", "/auth/sign-in", "/donation-info"];
-
-// export default function Navbar() {
-//   const [mobileOpen, setMobileOpen] = React.useState(false);
-
-//   const handleDrawerToggle = () => {
-//     setMobileOpen((prevState) => !prevState);
-//   };
-
-//   const drawer = (
-//     <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-//       <Link href="/" passHref>
-//         <Button color="secondary">
-//           <Image src={logo} alt="logo" width={50} height={50} />
-//         </Button>
-//       </Link>
-//       <Divider />
-//       <List>
-//         {navItems.map((item, index) => (
-//           <Grid item key={item}>
-//             {index === 2 ? <CategoryDropDownMenu /> : null} {/* Use CategoryDropDownMenu component */}
-//             <Link href={links[index]} passHref>
-//               <Button key={item} sx={{ color: "#000" }}>
-//                 {item}
-//               </Button>
-//             </Link>
-//           </Grid>
-//         ))}
-//         <Grid item>
-//           <ProfileDropDownMenu /> {/* Use ProfileDropDownMenu component */}
-//         </Grid>
-//       </List>
-//     </Box>
-//   );
-
-//   return (
-//     <Box sx={{ display: "flex" }}>
-//       <CssBaseline />
-//       <AppBar component="nav">
-//         <Toolbar>
-//           <IconButton
-//             color="inherit"
-//             aria-label="open drawer"
-//             edge="start"
-//             onClick={handleDrawerToggle}
-//             sx={{ mr: 2, display: { sm: "none" } }}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           <Box sx={{ flexGrow: 1 }}>
-//             <Link href="/" passHref>
-//               <Button color="secondary">
-//                 <Image src={logo} alt="logo" width={50} height={50} />
-//               </Button>
-//             </Link>
-//           </Box>
-//           <Box sx={{ display: { xs: "none", sm: "block" } }}>
-//             <Grid container spacing={2}>
-//               {navItems.map((item, index) => (
-//                 <Grid item key={item} sx={{ display: "flex" }}>
-//                   {index === 2 ? <CategoryDropDownMenu /> : null} {/* Use CategoryDropDownMenu component */}
-//                   <Link href={links[index]} passHref>
-//                     <Button key={item} sx={{ color: "#fff" }}>
-//                       {item}
-//                     </Button>
-//                   </Link>
-//                 </Grid>
-//               ))}
-//               <Grid item>
-//                 <ProfileDropDownMenu /> {/* Use ProfileDropDownMenu component */}
-//               </Grid>
-//             </Grid>
-//           </Box>
-//         </Toolbar>
-//       </AppBar>
-//       <nav>
-//         <Drawer
-//           variant="temporary"
-//           open={mobileOpen}
-//           onClose={handleDrawerToggle}
-//           ModalProps={{
-//             keepMounted: true, // Better open performance on mobile.
-//           }}
-//           sx={{
-//             display: { xs: "block", sm: "none" },
-//             "& .MuiDrawer-paper": {
-//               boxSizing: "border-box",
-//               width: drawerWidth,
-//             },
-//           }}
-//         >
-//           {drawer}
-//         </Drawer>
-//       </nav>
-//       <Box component="main">
-//         <Toolbar />
-//       </Box>
-//     </Box>
-//   );
-// }
-
-
-
-
 "use client";
 
 import Link from "next/link";
@@ -293,7 +175,7 @@ export default function Navbar() {
           </Grid>
         ))} 
         <Grid item>
-          <ProfileDropDownMenu /> {/* Use ProfileDropDownMenu component */}
+          <ProfileDropDownMenu /> 
         </Grid>
       </List>
     </Box>
@@ -333,7 +215,7 @@ export default function Navbar() {
                 </Grid>
               ))}
               <Grid item>
-                <ProfileDropDownMenu /> {/* Use ProfileDropDownMenu component */}
+                <ProfileDropDownMenu /> 
               </Grid>
             </Grid>
           </Box>
--- components/ProfileDropDownMenu.tsx
+++ components/ProfileDropDownMenu.tsx
@@ -1,13 +1,63 @@
+// import * as React from 'react';
+// import Button from '@mui/material/Button';
+// import Menu from '@mui/material/Menu';
+// import MenuItem from '@mui/material/MenuItem';
+
+// export default function ProfileDropDownMenu() {
+//   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+//   const open = Boolean(anchorEl);
+
+//   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+//     setAnchorEl(event.currentTarget);
+//   };
+
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
+
+//   return (
+//     <div>
+//       <Button
+//         id="profile-menu-button"
+//         aria-controls={open ? 'profile-menu' : undefined}
+//         aria-haspopup="true"
+//         aria-expanded={open ? 'true' : undefined}
+//         onClick={handleClick}
+//         color="inherit"
+//       >
+//         Profile
+//       </Button>
+//       <Menu
+//         id="profile-menu"
+//         anchorEl={anchorEl}
+//         open={open}
+//         onClose={handleClose}
+//         MenuListProps={{
+//           'aria-labelledby': 'profile-menu-button',
+//         }}
+//       >
+//         <MenuItem onClick={handleClose}>Profile</MenuItem>
+//         <MenuItem onClick={handleClose}>My account</MenuItem>
+//         <MenuItem onClick={handleClose}>Logout</MenuItem>
+//       </Menu>
+//     </div>
+//   );
+// }
+
+
+
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle'; // Import the AccountCircle icon
 
 export default function ProfileDropDownMenu() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
@@ -17,16 +67,17 @@ export default function ProfileDropDownMenu() {
 
   return (
     <div>
-      <Button
-        id="profile-menu-button"
+      <IconButton
+        size="large"
+        edge="end"
+        aria-label="account of current user"
         aria-controls={open ? 'profile-menu' : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
         color="inherit"
       >
-        Profile
-      </Button>
+        <AccountCircle />
+      </IconButton>
       <Menu
         id="profile-menu"
         anchorEl={anchorEl}
@@ -38,7 +89,7 @@ export default function ProfileDropDownMenu() {
       >
         <MenuItem onClick={handleClose}>Profile</MenuItem>
         <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MenuItem onClick={handleClose}>Sign In</MenuItem>
       </Menu>
     </div>
   );
