099348e58ab7a84794fefb8d9806b0991a792fd9
Edwin Ho <edwinh300@gmail.com>
2024-03-08 15:07:43 -0800

    update styling for clarity

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { ChangeEventHandler, FormEventHandler, useState } from "react";
 // import styles from "./forgot-password-page.module.css";
-import { Container, Typography, Link, Button, Box, TextField } from '@mui/material';
+import { Container, Typography, Link, Button, Box, TextField, Paper } from '@mui/material';
 
 const ForgotPassword = () => {
   
@@ -27,27 +27,42 @@ const ForgotPassword = () => {
   return (
     <Container fixed maxWidth="lg" sx={{display: "flex", justifyContent: "center", alignItems: "center", bgcolor: '#12202d'}}>
 
+      <Paper
+        elevation={4}
+        sx={{
+          padding: "20px",
+          width: "400px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "2em",
+          marginBottom: "2em"
+        }}
+      >
+
       <Box  
         component="form"
         onSubmit={handleSubmit}
         noValidate
-        sx={{ mt: 1, width: "100%" }}>
+        sx={{ mt: 1, width: "100%"}}>
 
-          <Typography variant='h3' sx={{fontWeight: 'bold', fontSize: '1.8rem', marginBottom: '1rem', color: '#101c29'}}>
+          <Typography variant='h3' sx={{ fontFamily: 'Roboto, Helvetic, Arialsans-serif',fontWeight: 'bold', fontSize: '1.8rem', marginBottom: '1rem', color: 'black'}}>
           Forgot Password
           </Typography>
 
           <TextField
+            variant="outlined"
             label="Email"
             type="email"
             name="email"
             value={email}
             onChange={handleChange}
           />
-          <Button component='a' type="submit" variant="outlined">
+          <Button component='a' type="submit" variant="outlined" sx={{marginLeft: "1em",borderColor: "black"}}>
             Submit
           </Button>
       </Box>
+      </Paper>
     </Container>
   );
 };
