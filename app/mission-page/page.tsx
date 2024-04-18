94a08c75ec61ed47e060d51a171d788fb73aec50
JoyZhang2023 <137982978+JoyZhang2023@users.noreply.github.com>
2024-04-17 22:41:39 -0700

    placeholder mission page

---
+++ app/mission-page/page.tsx
@@ -0,0 +1,14 @@
+// Placeholder page for mission
+
+import {Typography} from '@mui/material';
+
+const Mission = () => {
+
+    return (
+        <Typography component='h1' variant='h3' sx={{color: 'white'}}>Our Mission</Typography>
+        // Save for mission content
+        
+    );
+  };
+  
+  export default Mission;
