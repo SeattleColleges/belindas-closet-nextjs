1d4ee7547d8795cddec20bb8b6254e9ec1d5c7a0
Isaac J <duckhunterjr@gmail.com>
2024-04-19 13:38:20 -0700

    add tests to add-product-page

---
+++ tests/unit/pages/add-product-page/page.test.tsx
@@ -0,0 +1,34 @@
+import AddProduct from '@/app/add-product-page/page'
+import { ProductTypeList } from '@/app/add-product-page/product-prop-list'
+import { fireEvent, render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+
+describe('add-product-page tests', () => {
+    // Arrange
+    beforeEach(() => {
+    render(<AddProduct />)
+  })
+  it('contains title and submit button', () => {
+    // Act
+    const title = screen.getByText(/Add a Product/i )
+    const submitBtn = screen.getByRole('button', { name: /submit/i })
+    
+    // Assert
+    expect(title).toBeInTheDocument()
+    expect(submitBtn).toBeInTheDocument()
+    
+  })
+
+  it('checks if each product type option are rendered after clicking', () => {
+    // Act 
+    const selectType = screen.getByLabelText(/Product Type/i)
+    fireEvent.mouseDown(selectType)
+
+    // Assert
+    Object.values(ProductTypeList).forEach((type) => {
+      const option = screen.getByRole('option', { name: new RegExp(type, 'i') })
+      expect(option).toBeInTheDocument()
+    })
+  })
+})
