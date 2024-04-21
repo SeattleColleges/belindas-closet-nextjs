c80d266f227154723ecd183ece18379bd0923f88
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-04-21 00:08:51 -0700

    contact commit

--- app/Contact-Page/page.tsx
+++ app/Contact-Page/page.tsx
@@ -9,4 +9,4 @@ const Contact = () => {
     );
   };
 
-  export default Contact;
+  export default Contact; 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,8 +21,8 @@ import MenuIcon from "@mui/icons-material/Menu";
 import CategoryDropDownMenu from "./CategoryDropDownMenu";
 
 const drawerWidth = 240;
-const navItems = ["Home", "Sign In", "Donation", "Mission"];
-const links = ["/", "/auth/sign-in", "/donation-info", "/mission-page"];
+const navItems = ["Home", "Sign In", "Donation", "Mission","Contact"];
+const links = ["/", "/auth/sign-in", "/donation-info", "/mission-page","contact-page"];
 
 export default function Navbar() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
