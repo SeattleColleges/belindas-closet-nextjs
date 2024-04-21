f40a4af18b3e11e7746027f25fabbbc1fa96d4fc
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-04-21 00:33:19 -0700

    fixed capitalization

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ import CategoryDropDownMenu from "./CategoryDropDownMenu";
 
 const drawerWidth = 240;
 const navItems = ["Home", "Sign In", "Donation", "Mission","Contact"];
-const links = ["/", "/auth/sign-in", "/donation-info", "/mission-page","contact-page"];
+const links = ["/", "/auth/sign-in", "/donation-info", "/mission-page","contact-page","adminpage"];
 
 export default function Navbar() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
