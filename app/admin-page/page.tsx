0cf7986c9963af932585d932cd754f33ec5760ac
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-05-02 00:42:23 -0700

    belindas-closet-nextjs_5_334_add-a-link-button-to-visualization-dashboard (#335)
    * the dashboard link has been added
    
    * UI styling has been added

--- app/admin-page/page.tsx
+++ app/admin-page/page.tsx
@@ -1,4 +1,5 @@
 // import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
+import Button from '@mui/material/Button';
 
 const Admin = () => {
   const handleAddProduct = () => {
@@ -11,13 +12,11 @@ const Admin = () => {
       <p>FIX: allow only users with admin role to be routed to this page</p>
         
         {/* Changed button styling to match with Creator page's buttons */}
-
-        <a href="/add-product-page">
-        {/* <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}> */}
-        <button>
-          Add Product
-        </button>
-        </a>
+        <div>
+                <Button variant="contained" href="/add-product-page"> 
+                   Add Product
+                </Button>
+            </div>
       
         {/* <Link to="/add-product-page"> */}
         {/* <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}> */}
@@ -26,18 +25,23 @@ const Admin = () => {
         </button>
         {/* </Link> */}
 
-        <a href="/edit-user-role-page">
-        {/* <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}> */}
-        <button>
-          Edit User Roles
-        </button>
-        </a>
+        
+        <div>
+                <Button variant="contained" href="/edit-user-role-page"> 
+                    Contact Page
+                </Button>
+            </div>
       
-      {/* <Link to="/add-product-page"> */}
-        <button>
-         Contact page
-        </button>
-      {/* </Link> */}
+        <div>
+                <Button variant="contained" href="/contact-page"> 
+                    Contact Page
+                </Button>
+            </div>
+      <div>
+                <Button variant="contained" href="/dashboard"> 
+                    Dashboard
+                </Button>
+            </div>
     </div>
   );
 };
