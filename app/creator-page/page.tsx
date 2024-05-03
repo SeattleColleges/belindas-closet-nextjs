fa5b9e96c6c6aa6d4e2ef5d554ddb91b9166ecb4
NW Miller <nwmiller.516@gmail.com>
2024-05-02 21:18:01 -0700

    Integrate MUI on Creator page

--- app/creator-page/page.tsx
+++ app/creator-page/page.tsx
@@ -1,31 +1,41 @@
-import Button from '@mui/material/Button';
+'use client'
+import { createContext } from 'react';
+
+import { Button, Typography } from '@material-ui/core';
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+
 const Creator = () => {
-    // Temporary boilerplate code to make it compile
     return (
         <div>
-        <div >
-        <div>
-                <Button variant="contained" href="/"> 
+            <Typography
+                variant="h4"
+                component="h1"
+                gutterBottom
+            >
+                Welcome to the CREATOR PAGE
+            </Typography>
+
+            <Typography
+                variant="body1"
+                gutterBottom
+            >
+                FIX: Creator Placeholder Text goes here
+            </Typography>
+
+            <ButtonGroup
+                color="primary"
+                variant="contained"
+            >
+
+                <Button href="/add-product-page">
+                    Add Product
+                </Button>
+
+                <Button href="/">
                     All Products
                 </Button>
-            </div>
-            <div>
-                <Button variant="contained" href="/add-product-page"> 
-                    Add Products
-                </Button>
-            </div>
 
-            <div>
-                <Button variant="contained" href="/dashboard"> 
-                    Dashboard
-                </Button>
-            </div>
-        <>
-     </>
-        </div>
-
-            <h1>Placeholder creators page.</h1>
-            <p>Place holder text to fill in the space.</p>
+            </ButtonGroup>
         </div>
     );
 };
