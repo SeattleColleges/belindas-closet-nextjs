ff8ca65f556a9d9b91d086804bd97180a504e37b
Hanan Osman <hanan.osman@seattlecolleges.edu>
2024-05-07 19:13:36 -0700

    Update shoe sizing
    Added sizes 4 and 13, and half-sizes.

--- app/add-product-page/product-prop-list.tsx
+++ app/add-product-page/product-prop-list.tsx
@@ -16,7 +16,7 @@ export enum ProductGenderList {
   NON_BINARY = "NON_BINARY",
 }
 
-export const ProductSizeShoeList = [5, 6, 7, 8, 9, 10, 11, 12];
+export const ProductSizeShoeList = [4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13];
 
 export enum ProductSizesList {
   XS = "XS",
