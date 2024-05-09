7e71435b31ab04ae1476c631836f6e74788068ee
hanan o <77591083+heosman@users.noreply.github.com>
2024-05-09 13:10:38 -0700

    belindas-closet-nextjs_6_351_update-product-sizing (#355)
    * Update shoe sizing
    
    Added sizes 4 and 13, and half-sizes.
    
    * Add odd numbers for waist lengths of pants
    
    * Added odd numbers for inseam lengths for pants

--- app/add-product-page/product-prop-list.tsx
+++ app/add-product-page/product-prop-list.tsx
@@ -16,7 +16,7 @@ export enum ProductGenderList {
   NON_BINARY = "NON_BINARY",
 }
 
-export const ProductSizeShoeList = [5, 6, 7, 8, 9, 10, 11, 12];
+export const ProductSizeShoeList = [4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13];
 
 export enum ProductSizesList {
   XS = "XS",
@@ -27,6 +27,6 @@ export enum ProductSizesList {
   XXL = "XXL",
 }
 
-export const ProductSizePantsWaistList = [28, 30, 32, 34, 36, 38, 40, 42];
+export const ProductSizePantsWaistList = [28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
 
-export const ProductSizePantsInseamList = [28, 30, 32, 34, 36, 38, 40, 42];
+export const ProductSizePantsInseamList = [28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
