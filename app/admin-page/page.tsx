baa3b8d239820853b45718dc35edade8e064cd79
NW Miller <36825393+nwm516@users.noreply.github.com>
2024-05-21 21:08:24 -0700

    belindas-closet-nextjs_6_321_mui-admin-page (#342)
    * Integrate MUI to Admin page
    
    * Update dependencies
    
    * Add material-ui/core
    
    * Update MUI imports
    
    * Update package.json
    
    * Delete yarn.lock

--- app/admin-page/page.tsx
+++ app/admin-page/page.tsx
@@ -1,5 +1,11 @@
 // import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
+
+'use client'
+import { createContext } from 'react';
+
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import ButtonGroup from '@mui/material/ButtonGroup'
 
 const Admin = () => {
   const handleAddProduct = () => {
@@ -8,40 +14,40 @@ const Admin = () => {
 
   return (
     <div>
-      <h1> Welcome to the PAGE      </h1>
-      <p>FIX: allow only users with admin role to be routed to this page</p>
-        
-        {/* Changed button styling to match with Creator page's buttons */}
-        <div>
-                <Button variant="contained" href="/add-product-page"> 
-                   Add Product
-                </Button>
-            </div>
-      
-        {/* <Link to="/add-product-page"> */}
-        {/* <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', marginTop: '20px' }}> */}
-        <button>
-          All Products
-        </button>
-        {/* </Link> */}
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
+      >
+        Welcome to the ADMIN PAGE
+      </Typography>
 
-        
-        <div>
-                <Button variant="contained" href="/edit-user-role-page"> 
-                    Contact Page
-                </Button>
-            </div>
-      
-        <div>
-                <Button variant="contained" href="/contact-page"> 
-                    Contact Page
-                </Button>
-            </div>
-      <div>
-                <Button variant="contained" href="/dashboard"> 
-                    Dashboard
-                </Button>
-            </div>
+      <Typography
+        variant="body1"
+        gutterBottom
+      >
+        FIX: allow only users with admin role to be routed to this page
+      </Typography>
+
+
+      <ButtonGroup
+        color="primary"
+        variant="contained"
+      >
+
+        <Button href="/add-product-page">
+          Add Product
+        </Button>
+
+        <Button href="/">
+          All Products
+        </Button>
+
+        <Button href="/edit-user-role-page">
+          Edit User Roles
+        </Button>
+
+      </ButtonGroup>
     </div>
   );
 };
