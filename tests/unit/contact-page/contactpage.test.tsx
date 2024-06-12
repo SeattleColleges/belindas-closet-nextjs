fb2c324d3c58bc6781a2749a602483cbf51860cf
Lilith Caldwell <purpleirishlilli69@gmail.com>
2024-06-12 01:02:33 -0700

    Contact-page test (#451)
    unit test for contact page

---
+++ tests/unit/contact-page/contactpage.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Contact from '../../../app/contact-page/page';
+import { Typography, Box, TextField, Button } from '@mui/material';
+
+jest.mock('@mui/material', () => ({
+  ...jest.requireActual('@mui/material'),
+  Typography: jest.fn((props: any) => <div>{props.children}</div>),
+  Box: jest.fn((props: any) => <div>{props.children}</div>),
+  TextField: jest.fn((props: any) => <input {...props} />),
+  Button: jest.fn((props: any) => <button {...props} />),
+}));
+
+describe('Contact component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('renders correctly', () => {
+    render(<Contact />);
+    expect(Typography).toHaveBeenCalledTimes(8);
+    expect(Box).toHaveBeenCalledTimes(3);
+    expect(TextField).toHaveBeenCalledTimes(4); // Adjusted the expectation to match the number of TextField components
+    expect(Button).toHaveBeenCalledTimes(1);
+  });
+});
