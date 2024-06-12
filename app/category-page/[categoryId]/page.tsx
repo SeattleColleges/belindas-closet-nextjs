ed1fdab866d4f5fee87122c8b3d1db98e9cd9bca
Intisar Osman <intisar.Osman@seattlecolleges.edu>
2024-06-11 22:42:36 -0700

    belindas-closet-nextjs_8_448_simplify-product-description
    Resolves #448 #449
    
    This PR minimizes product descriptions  on the category page (to just the gender and description, and doesn't include sizes). The product cards are smaller now so there can be more columns.

--- app/category-page/[categoryId]/page.tsx
+++ app/category-page/[categoryId]/page.tsx
@@ -76,15 +76,15 @@ const ViewProduct = ({ categoryId }: { categoryId: string }) => {
       </Typography>
       <Grid container spacing={2}>
         {filteredProducts.map((product, index) => (
-          <Grid item key={index} xs={12} sm={6} md={4}>
+          <Grid item key={index} xs={12} sm={4} md={2.5}>
             <ProductCard
               image={logo}
               categories={product.productType}
               gender={product.productGender}
-              sizeShoe={product.productSizeShoe}
-              size={product.productSizes}
-              sizePantsWaist={product.productSizePantsWaist}
-              sizePantsInseam={product.productSizePantsInseam}
+              sizeShoe=''
+              size=''
+              sizePantsWaist=''
+              sizePantsInseam=''
               description={product.productDescription}
               href={`/category-page/${categoryId}/products/${product._id}`} // Construct the URL
               _id={product._id}
