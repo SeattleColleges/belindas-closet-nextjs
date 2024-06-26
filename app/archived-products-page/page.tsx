276268a8b8c6ddfc0d16ddf4c1eec869334e8700
Intisar Osman <intisar.Osman@seattlecolleges.edu>
2024-06-25 23:30:30 -0700

    Created Archived Products Page

---
+++ app/archived-products-page/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Typography, Drawer, List, ListItem, ListItemText, IconButton } from "@mui/material";
+import { SetStateAction, useState } from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+
+const ArchivedProducts = () => {
+    return (
+        <Typography component="h1" variant="h3">
+          Archived Products
+        </Typography>
+      );
+    };
+
+export default ArchivedProducts;
