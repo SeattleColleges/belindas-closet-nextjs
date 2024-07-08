891061f05dd2bbe32bc112d3570d245cd34466b7
intisarosman1 <77607212+intisarosman1@users.noreply.github.com>
2024-07-08 13:38:25 -0700

    belindas-closet-nextjs_9_475-home-mission-donation-page-responsive-ui (#476)
    Resolves #475
    
    This PR cleans up the UI for mobile screens by centering the Home page logo above the Sign Up and Sign In buttons properly (the logo was off to the side before). I also added margin above the headers on the Donation and Mission page because they were very close under the nav bar.
    
    I noticed an unused CSS file was made for Donation page, so I deleted it because we are switching to Material UI.

--- app/mission-page/page.tsx
+++ app/mission-page/page.tsx
@@ -4,7 +4,7 @@ import { Typography } from "@mui/material";
 
 const Mission = () => {
   return (
-    <Typography component="h1" variant="h3">
+    <Typography component="h1" variant="h3" sx={{ mt: 3 }}>
       Our Mission
     </Typography>
     // Save for mission content
