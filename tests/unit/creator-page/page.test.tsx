2e51607066bfc52c769927118c5e9e5d96777c33
Asfand Tanwer <asfandtanwer@yahoo.com>
2024-07-11 00:30:58 -0700

    "update jest testing for creator page"

--- tests/unit/creator-page/page.test.tsx
+++ tests/unit/creator-page/page.test.tsx
@@ -19,15 +19,20 @@ describe('Creator component tests', () => {
       // Check for the links and their href attributes
       const addButton = screen.getByRole('link', { name: /Add Product/i });
       const allProductsButton = screen.getByRole('link', { name: /All Products/i });
+      const archivedProductsButton = screen.getByRole('link', { name: /Archived Products/i })
       
       expect(addButton).toBeInTheDocument();
       expect(addButton).toHaveAttribute('href', '/add-product-page');
   
       expect(allProductsButton).toBeInTheDocument();
-      expect(allProductsButton).toHaveAttribute('href', '/');
-  
+      expect(allProductsButton).toHaveAttribute('href', '/category-page/all-products');
+
+      expect(archivedProductsButton).toBeInTheDocument();
+      expect(archivedProductsButton).toHaveAttribute('href', '/archived-products-page');
+
       // Simulate link clicks
       await userEvent.click(addButton);
       await userEvent.click(allProductsButton);
+      await userEvent.click(archivedProductsButton);
     });
   });
