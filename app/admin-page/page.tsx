4906304bcd0f39c9dacb527b86893193046c3fbf
intisarosman1 <77607212+intisarosman1@users.noreply.github.com>
2024-07-18 19:31:51 -0700

    belindas-closet-nextjs-500-admin-creator-page-responsive-ui (#501)
    * belindas-closet-nextjs-500-admin-creator-page-responsive-ui
    
    Resolves #500
    
    This PR ensures that the Admin and Creator pages have responsive UI and adjusts to mobile screen sizes (and tablet screens) well, with no cutoffs or the need to scroll horizontally.
    
    Test this PR by right clicking the page and using Inspect elements to enter the Toggle Device Toolbar and adjust the screen sizes.
    
    * Commit
    
    * Adjusted test and further UI appearance of pages

--- app/admin-page/page.tsx
+++ app/admin-page/page.tsx
@@ -1,16 +1,13 @@
-// import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
-
 'use client'
-import { createContext } from 'react';
 
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import ButtonGroup from '@mui/material/ButtonGroup'
+import { useMediaQuery, useTheme } from '@mui/material';
 
 const Admin = () => {
-  const handleAddProduct = () => {
-    // Add code here to handle the "Add Product" button click event
-  };
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <div>
@@ -18,39 +15,35 @@ const Admin = () => {
         variant="h4"
         component="h1"
         gutterBottom
+        sx={{ mt: 4, mb: isMobile ? 4 : 6 }}
       >
-        Welcome to the ADMIN PAGE
+        My Account
       </Typography>
-
-      <Typography
-        variant="body1"
-        gutterBottom
-      >
-        FIX: allow only users with admin role to be routed to this page
-      </Typography>
-
-
       <ButtonGroup
         color="primary"
         variant="contained"
+        sx={{ flexDirection: isMobile ? "column" : "row",
+          '& .MuiButton-root': {
+            borderRadius: '4px',
+            mr: isMobile ? 0 : 1,
+            ml: isMobile ? 0 : 1,
+            mt: isMobile ? 1 : 0,
+            mb: isMobile ? 2 : 0,
+          },
+         }}
       >
-
         <Button href="/add-product-page">
           Add Product
         </Button>
-
         <Button href="/category-page/all-products">
           All Products
         </Button>
-
         <Button href="/edit-user-role-page">
           Edit User Roles
         </Button>
-
         <Button href="/archived-products-page">
           Archived Products
         </Button>
-
       </ButtonGroup>
     </div>
   );
--- app/creator-page/page.tsx
+++ app/creator-page/page.tsx
@@ -1,45 +1,46 @@
 'use client'
-import { createContext } from 'react';
 
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import { useMediaQuery, useTheme } from '@mui/material';
 
 const Creator = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
         <div>
             <Typography
                 variant="h4"
                 component="h1"
                 gutterBottom
+                sx={{ mt: 4, mb: isMobile ? 4 : 6 }}
             >
-                Welcome to the CREATOR PAGE
+                My Account
             </Typography>
-
-            <Typography
-                variant="body1"
-                gutterBottom
-            >
-                FIX: Creator Placeholder Text goes here
-            </Typography>
-
             <ButtonGroup
                 color="primary"
                 variant="contained"
+                sx={{ flexDirection: isMobile ? "column" : "row",
+                    '& .MuiButton-root': {
+                        borderRadius: '4px',
+                        mr: isMobile ? 0 : 1,
+                        ml: isMobile ? 0 : 1,
+                        mt: isMobile ? 1 : 0,
+                        mb: isMobile ? 2 : 0,
+                        },
+                 }}
             >
-
                 <Button href="/add-product-page">
                     Add Product
                 </Button>
-
                 <Button href="/category-page/all-products">
                     All Products
                 </Button>
-
                 <Button href="/archived-products-page">
                     Archived Products
                 </Button>
-
             </ButtonGroup>
         </div>
     );
