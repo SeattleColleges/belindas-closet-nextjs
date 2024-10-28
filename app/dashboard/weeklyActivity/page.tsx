590a4aa51e81eb8bd6cc9623e985b16304510ae6
NW Miller <nwmiller.516@gmail.com>
2024-10-27 22:38:47 -0700

    Update page.tsx
    Update page.tsx for clashing properties with dashboard

--- app/dashboard/weeklyActivity/page.tsx
+++ app/dashboard/weeklyActivity/page.tsx
@@ -1,35 +1,28 @@
 "use client";
-import {Chart as ChartJS, registerables} from 'chart.js/auto'
-import {Bar} from "react-chartjs-2";
-import {Chart} from "chart.js";
+import { Chart as ChartJS, registerables } from 'chart.js/auto';
+import { Bar } from "react-chartjs-2";
 import React from "react";
 
-function generatedDays(length:number):string[] {
+function generatedDays(length: number): string[] {
     const now = new Date();
     return Array.from({ length: length }, (_, i) => {
         const date = new Date(now);
         date.setDate(now.getDate() - (89 - i));
         return date.toISOString().split('T')[0];
-    })
+    });
 }
 
-export const WeeklyActivity = () =>  {
+export const WeeklyActivity = () => {
     const [firstBarRange, setFirstBarRange] = React.useState<number[]>([0, 30]);
     const [secondBarRange, setSecondBarRange] = React.useState<number[]>([0, 7]);
 
-    /*
-        The below contains generated data
-
-        TODO: Replace with real data
-    */
-
     // Date generated labels
-    const generated90dayLabels = generatedDays(90)
-    const generated7dayLabels = generatedDays(21)
+    const generated90dayLabels = generatedDays(90);
+    const generated7dayLabels = generatedDays(21);
 
     // Generated data
-    const sim90Days = Array.from({ length: 90 }, () => Math.floor(Math.random() * 100))
-    const sim7Days = Array.from({ length: 21 }, () => Math.floor(Math.random() * 100))
+    const sim90Days = Array.from({ length: 90 }, () => Math.floor(Math.random() * 100));
+    const sim7Days = Array.from({ length: 21 }, () => Math.floor(Math.random() * 100));
 
     // Data for first bar
     const presets = {
@@ -49,17 +42,18 @@ export const WeeklyActivity = () =>  {
                 label: 'Data for last 90 days',
                 data: sim90Days.slice(firstBarRange[0], firstBarRange[1]), // Replace with your actual data
                 backgroundColor: Array.from({ length: sim90Days.slice(firstBarRange[0], firstBarRange[1]).length }, (_, i) => i % 2 === 0 ? 'rgba(0, 91, 170, 1)' : 'rgba(139, 199, 81, 1)'),
+                barThickness: 8, // Moved here if needed
             },
         ],
     };
 
-    Chart.register(...registerables);
+    ChartJS.register(...registerables);
 
     return (
         <>
-            <div style={{width: "100vw", height: "300px", paddingLeft: "3rem", paddingRight: "3rem"}}>
-                <h5 style={{textAlign: "left"}}>
-                    <span onClick={() => setSecondBarRange([secondBarRange[0]+7, secondBarRange[1]+7])} style={{
+            <div style={{ width: "100vw", height: "300px", paddingLeft: "3rem", paddingRight: "3rem" }}>
+                <h5 style={{ textAlign: "left" }}>
+                    <span onClick={() => setSecondBarRange([secondBarRange[0] + 7, secondBarRange[1] + 7])} style={{
                         display: "inline-block",
                         marginRight: "5px",
                         backgroundColor: "white",
@@ -71,7 +65,7 @@ export const WeeklyActivity = () =>  {
                     }}>
                         ‹
                     </span>
-                    <span onClick={() => setSecondBarRange([secondBarRange[0]-7, secondBarRange[1]-7])} style={{
+                    <span onClick={() => setSecondBarRange([secondBarRange[0] - 7, secondBarRange[1] - 7])} style={{
                         display: "inline-block",
                         marginRight: "5px",
                         backgroundColor: "white",
@@ -92,7 +86,7 @@ export const WeeklyActivity = () =>  {
                         maintainAspectRatio: false,
                         plugins: {
                             legend: {
-                                display: false
+                                display: false,
                             },
                             title: {
                                 display: true,
@@ -110,10 +104,10 @@ export const WeeklyActivity = () =>  {
                     }}
                 />
             </div>
-            <hr style={{marginLeft: "5rem", marginRight: "5rem", marginBottom: "4rem", marginTop: "4rem"}}/>
-            <div style={{width: "100vw", height: "300px", paddingLeft: "3rem", paddingRight: "3rem"}}>
-                <h5 style={{textAlign: "left"}}>
-                    <span onClick={() => setFirstBarRange([firstBarRange[0]+30, firstBarRange[1]+30])} style={{
+            <hr style={{ marginLeft: "5rem", marginRight: "5rem", marginBottom: "4rem", marginTop: "4rem" }} />
+            <div style={{ width: "100vw", height: "300px", paddingLeft: "3rem", paddingRight: "3rem" }}>
+                <h5 style={{ textAlign: "left" }}>
+                    <span onClick={() => setFirstBarRange([firstBarRange[0] + 30, firstBarRange[1] + 30])} style={{
                         display: "inline-block",
                         marginRight: "5px",
                         backgroundColor: "white",
@@ -125,7 +119,7 @@ export const WeeklyActivity = () =>  {
                     }}>
                         ‹
                     </span>
-                    <span onClick={() => setFirstBarRange([firstBarRange[0]-30, firstBarRange[1]-30])} style={{
+                    <span onClick={() => setFirstBarRange([firstBarRange[0] - 30, firstBarRange[1] - 30])} style={{
                         display: "inline-block",
                         marginRight: "5px",
                         backgroundColor: "white",
@@ -144,18 +138,15 @@ export const WeeklyActivity = () =>  {
                     options={{
                         responsive: true,
                         maintainAspectRatio: false,
-                        barThickness: 8,
                         plugins: {
                             legend: {
-                                display: false
+                                position: 'top',
+                                display: false, // If you want to hide it, otherwise set to true
                             },
                             title: {
                                 display: true,
                                 text: 'Bar Chart of Last 30 Days',
                             },
-                        },
-                        legend: {
-                            position: 'top',
                         },
                         scales: {
                             x: {
@@ -175,7 +166,7 @@ export const WeeklyActivity = () =>  {
                 />
             </div>
         </>
-    )
+    );
 }
 
 export default WeeklyActivity;
