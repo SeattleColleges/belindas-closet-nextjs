f64db7a1c3d9879fa3b7876a4d36c63e8b6cd118
Nicholas Wade Miller <36825393+nwm516@users.noreply.github.com>
2024-12-05 10:47:11 -0800

    Fix WrapperDiv, Foot, and Navbar (#564)
    Fixed bugs associated with WrapperDiv, Footer, and Navbar components

--- components/WrapperDiv.tsx
+++ components/WrapperDiv.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { Box, BoxProps } from "@mui/material";
 
-const styles = {
-  borderRadius: "0.5rem",
-  width: "100%",
-  height: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  gap: "1rem",
-  textAlign: "center",
-  paddingBottom: 5
-};
-
-const CustomCardContent: React.FC<BoxProps> = ({ children }) => {
-  return <Box sx={styles}>{children}</Box>;
+const CustomCardContent: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        width: "100%",
+        padding: "1rem",
+      }}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default CustomCardContent;
