c4a2f8458c1a2a3df66edd65fffd2e3e87f96ffa
Luiz Ramos <luiz.matthew187@gmail.com>
2025-03-08 23:42:20 -0800

    Added a search feature

--- app/edit-user-role-page/page.tsx
+++ app/edit-user-role-page/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import UserCard from "../../components/UserCard";
-import {Box, Stack, Typography} from "@mui/material";
+import {Box, Stack, TextField, Typography} from "@mui/material";
 import UnauthorizedPageMessage from "@/components/UnauthorizedPageMessage";
+import Grid from "@mui/material/Grid";
 
 const URL = process.env.BELINDAS_CLOSET_PUBLIC_API_URL;
 
@@ -55,6 +56,7 @@ async function fetchUser(setUserInfo: (userInfo: User[]) => void, userToken: JWT
 const EditUserRolePage = () => {
   const [userInfo, setUserInfo] = useState<User[]>([]);
   const [userRole, setUserRole] = useState("");
+  const [search, setSearch] = useState<String>("");
 
   useEffect(() => {
     const userToken: JWToken = localStorage.getItem("token")
@@ -75,8 +77,25 @@ const EditUserRolePage = () => {
         <Typography component="h1" variant="h4" textAlign={"center"}>
           User Management
         </Typography>
+        <Grid item xs={12} sm={4} md={3}>
+          <TextField
+              id="outlined-basic"
+              label="Search"
+              variant="outlined"
+              sx={{ background: "white", marginLeft: 2 }}
+              onChange={(e) => setSearch(e.target.value)}
+          />
+        </Grid>
         <Box sx={{ display: "flex" }}>
-          {userInfo.map((user, index) => (
+          {userInfo.filter(item => {
+            const searchLower = search.toLowerCase();
+            return (
+                (item.firstName.toLowerCase() + " " +
+                item.lastName.toLowerCase()).includes(searchLower) ||
+                item.email.toLowerCase().includes(searchLower) ||
+                item.role.toLowerCase().includes(searchLower)
+            );
+          }).map((user, index) => (
             <UserCard user={user} key={index} />
           ))}
         </Box>
