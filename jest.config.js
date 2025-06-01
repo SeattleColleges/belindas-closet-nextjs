72a28aa53cbfe7f7330784984bacf1ec1a9624e1
elida <el_riber@hotmail.com>
2025-06-01 16:14:37 -0700

    test complete

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,17 @@ const createJestConfig = nextJest({
     dir: './',
 });
 
-// Add any custom config to be passed to Jest
+
 /** @type {import('jest').Config} */
 const config = {
-    // Add more setup options before each test is run
+    
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
     preset: 'ts-jest',
+    moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1', 
+  },
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+
 module.exports = createJestConfig(config);
