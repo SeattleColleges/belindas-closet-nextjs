9d436dd5d7afc4a3b45ed5b521b737075ea7de16
Asfand Tanwer <asfandtanwer@yahoo.com>
2025-06-18 23:55:49 -0700

    Fixed conflicts

--- jest.config.js
+++ jest.config.js
@@ -4,15 +4,16 @@ const createJestConfig = nextJest({
   dir: './',
 });
 
+// Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
     // Add more setup options before each test is run
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // ✅ Only use your setup file
     testEnvironment: 'jest-environment-jsdom',
     preset: 'ts-jest',
     moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/$1', // ✅ for root-based path alias
     },
 };
-
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
