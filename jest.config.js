34df11f3b9695bfbcd770a9e15d6164e81889aea
el-riber <125428353+el-riber@users.noreply.github.com>
2025-06-19 00:07:20 -0700

    Unit Test for ProductDetail Page (#672)
    * test complete
    
    * setup jest
    
    * Fixed conflicts
    
    * renamed variable to fix error
    
    ---------
    
    Co-authored-by: Asfand Tanwer <asfandtanwer@yahoo.com>

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,16 @@ const createJestConfig = nextJest({
   dir: './',
 });
 
+// Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // ✅ Only use your setup file
-  testEnvironment: 'jest-environment-jsdom',
-  preset: 'ts-jest',
-  moduleNameMapper: {
+    // Add more setup options before each test is run
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // ✅ Only use your setup file
+    testEnvironment: 'jest-environment-jsdom',
+    preset: 'ts-jest',
+    moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1', // ✅ for root-based path alias
-  },
+    },
 };
-
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
