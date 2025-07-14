896d767d914b150bbf576f5737419a4b87babe6d
el-riber <125428353+el-riber@users.noreply.github.com>
2025-07-14 12:35:07 -0700

    test complete (#668)

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,11 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const customJestConfig = {
     // Add more setup options before each test is run
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // ✅ Only use your setup file
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], //Only use your setup file
     testEnvironment: 'jest-environment-jsdom',
     preset: 'ts-jest',
     moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1', // ✅ for root-based path alias
+    '^@/(.*)$': '<rootDir>/$1', // for root-based path alias
     },
 };
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
